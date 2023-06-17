652e1d34b65be2ccfdfd9b667e2edcd7e5b53806
DmitryShabucky <130282917+DmitryShabucky@users.noreply.github.com>
2023-06-17 17:27:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,35 +2,48 @@ import telebot
 
 from config import *
 from extensions import *
+from markup import *
 
 bot = telebot.TeleBot(TOKEN)
-
-
-@bot.message_handler(commands=['start', 'help'])
-def message(message: telebot.types.Message):
-    text = "Какие валюты хотите конвертировать?\nФормат ввода:\n<валюта>\
-<в какую валюту><количество валюты>\nПример ввода: рубль доллар 100\nСписок доступных валют: /values"
-    bot.reply_to(message, text)
-
 
 @bot.message_handler(commands=['values'])
 def currenciec(message: telebot.types.Message):
     text = 'Список доступных валют: '
     for cur in currency.keys():
         text = '\n'.join((text, cur))
-    bot.reply_to(message, text)
+    bot.reply_to(message, f'{text}\n/convert - конвертер')
 
+@bot.message_handler(commands=['start', 'help'])
+def start(message: telebot.types.Message):
+    text = 'Начнем конвертацию валют! \n/convert - конвертер'
+    bot.send_message(message.chat.id, text)
 
-@bot.message_handler(content_types=['text'])
-def converter(message: telebot.types.Message):
-    values = message.text.lower()
-    values = values.split(' ')
+@bot.message_handler(commands=['convert'])
+def values(message: telebot.types.Message):
+    text = 'Выберете вылюту для конвертации: '
+    bot.send_message(message.chat.id, text, reply_markup=create_markup())
+    bot.register_next_step_handler(message, base_handler)
+
+def base_handler(message:telebot.types.Message):
+    base = message.text.lower()
+    bot.send_message(message.chat.id, 'В какую валюту конвертировать?', reply_markup=create_markup(base))
+    bot.register_next_step_handler(message, quote_handler, base)
+
+def quote_handler(message: telebot.types.Message, base):
+    quote = message.text.lower()
+    bot.send_message(message.chat.id, 'Сколько конвертировать?')
+    bot.register_next_step_handler(message, amount_handler, base, quote)
+
+def amount_handler(message:telebot.types.Message, base,quote):
+    amount = message.text
+    values = base, quote, amount
+
     try:
         text = CurrencyExchange.get_price(values)
-        bot.send_message(message.chat.id, text)
+        bot.send_message(message.chat.id, 'Результат:\n\n' + text + '\n\n/convert - конвертер')
     except APIExceptions as e:
          bot.reply_to(message, f"Ошибка пользователя.\n{e}")
     except Exception as e:
-        bot.reply_to(message, f"Ошибка сервера.\n{e}")
+        bot.reply_to(message, f"Ошибка сервера.\n{e}\n/start")
 
 bot.polling(none_stop=True)
